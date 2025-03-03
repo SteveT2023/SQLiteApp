b9a46aeb2fecd276614f4bccbf356086c514f9bb
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-03-03 13:11:58 -0500

    Foundation

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: userInfo(),
+    );
+  }
+}
+
+class userInfo extends StatefulWidget {
+  const userInfo({super.key});
+
+  @override
+  State<userInfo> createState() => _userInfoState();
+}
+
+class _userInfoState extends State<userInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
